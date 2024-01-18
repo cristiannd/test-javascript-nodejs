8b308839759570d1594483d35a8be214acf2ff72
Cristian Donalicio <cristian.donalicio@claro.com.ar>
2024-01-18 16:02:20 -0300

    Test 6 resolved

--- test6_dotenv.js
+++ test6_dotenv.js
@@ -2,8 +2,30 @@
 // - La utilidad debe devolver un método config que lee el archivo .env
 //   y añade las variables de entorno que haya en el archivo al objeto process.env
 
-const dotenv = require("./dotenv.js")
-dotenv.config()
+// const dotenv = require("./dotenv.js")
+// dotenv.config()
 
-console.log(process.env.PORT)
-console.log(process.env.TOKEN)
+// console.log(process.env.PORT)
+// console.log(process.env.TOKEN)
+
+import fs from "node:fs";
+
+export function config({ path = ".env" } = {}) {
+  try {
+    const env = fs.readFileSync(path, "utf8");
+    const lines = env.split("\n");
+
+    lines.forEach((e) => {
+      const [key, ...value] = e.split("=");
+      const joinedValue = value.join("=");
+
+      process.env[key] = joinedValue;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+config();
+console.log('PORT:', process.env.PORT);
+console.log('TOKEN:', process.env.TOKEN);
