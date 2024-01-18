74fd029bb985f85726c67af842e8bae60686e466
Cristian Donalicio <cristian.donalicio@claro.com.ar>
2024-01-18 13:38:54 -0300

    Add test 5

---
+++ test5_delay.js
@@ -0,0 +1,5 @@
+// Escribe una función 'delay' que retorne una promesa que se resuelva después de 'n' milisegundos.
+
+export async function delay() {}
+
+delay(3000).then(() => console.log("Hola mundo"));
