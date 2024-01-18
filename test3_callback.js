852da73467fa24878d78f39b2af509eafd0b95e7
Cristian Donalicio <cristian.donalicio@claro.com.ar>
2024-01-18 13:11:15 -0300

    Test 3 resolved

--- test3_callback.js
+++ test3_callback.js
@@ -3,25 +3,48 @@
 // 3. Haz lo necesario para que el código para que siga funcionando con callback.
 // 4. Haz lo que concideres para mejorar la legibilidad.
 
-export function procesarArchivo() {
+import fs from "node:fs";
+
+// Callback
+export function procesarArchivo(callback) {
   fs.readFile("input.txt", "utf8", (error, contenido) => {
-    if (error) {
-      console.error("Error leyendo archivo:", error.message);
-      return false;
-    }
+    if (error) callback(`Error leyendo archivo: ${error.message}`);
 
-    setTimeout(() => {
-      const textoProcesado = contenido.toUpperCase();
+    const textoProcesado = contenido.toUpperCase();
 
-      fs.writeFile("output.txt", textoProcesado, (error) => {
-        if (error) {
-          console.error("Error guardando archivo:", error.message);
-          return false;
-        }
-
-        console.log("Archivo procesado y guardado con éxito");
-        return true;
-      });
-    }, 1000);
+    fs.writeFile("output.txt", textoProcesado, (error) => {
+      if (error)
+        callback(`Error guardando archivo: ${error.messageerror.message}`);
+      else callback(null, "Archivo procesado y guardado con éxito");
+    });
   });
 }
+
+const callbackFunc = (error, message) => {
+  if (error) console.error(error);
+  else console.log(message);
+};
+procesarArchivo(callbackFunc);
+
+// Async / Await
+export async function procesarArchivoPromise() {
+  let contenido = '';
+
+  try {
+    contenido = await fs.promises.readFile("input.txt", "utf8");
+  } catch (error) {
+    console.error("Error al leer el archivo:", error);
+    throw error;
+  }
+
+  const textoProcesado = contenido.toUpperCase();
+
+  try {
+    await fs.promises.writeFile("output.txt", textoProcesado);
+  } catch (error) {
+    console.error("Error al escribir en nuevo archivo:", error);
+    throw error;
+  }
+}
+
+await procesarArchivoPromise();
