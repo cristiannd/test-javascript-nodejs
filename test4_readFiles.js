99d6be46359d44db38db327f593966692c575747
Cristian Donalicio <cristian.donalicio@claro.com.ar>
2024-01-18 13:36:39 -0300

    Test 4 resolved

--- test4_readFiles.js
+++ test4_readFiles.js
@@ -3,11 +3,29 @@
 import fs from "node:fs";
 
 export function leerArchivos() {
-  const archivo1 = fs.readSync("archivo1.txt", "utf8");
-  const archivo2 = fs.readSync("archivo1.txt", "utf8");
-  const archivo3 = fs.readSync("archivo1.txt", "utf8");
+  console.time("leerArchivos");
+
+  const archivo1 = fs.readFileSync("archivo1.txt", "utf8");
+  const archivo2 = fs.readFileSync("archivo2.txt", "utf8");
+  const archivo3 = fs.readFileSync("archivo3.txt", "utf8");
+
+  console.timeEnd("leerArchivos");
 
   return `${archivo1} ${archivo2} ${archivo3}`;
 }
 
 leerArchivos();
+
+export async function leerArchivosMejorado() {
+  console.time("leerArchivosMejorado");
+  const [archivo1, archivo2, archivo3] = await Promise.all([
+    fs.promises.readFile("archivo1.txt", "utf8"),
+    fs.promises.readFile("archivo2.txt", "utf8"),
+    fs.promises.readFile("archivo3.txt", "utf8"),
+  ]);
+
+  console.timeEnd("leerArchivosMejorado");
+  return `${archivo1} ${archivo2} ${archivo3}`;
+}
+
+leerArchivosMejorado()
